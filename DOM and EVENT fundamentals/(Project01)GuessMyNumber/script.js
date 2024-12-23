4204417402392d59a303d2638638017b874ace9e
thexzabuza <ratherbasit253@gmail.com>
2024-12-23 14:59:58 +0530

    update 0.02 (guess the number)

--- DOM and EVENT fundamentals/(Project01)GuessMyNumber/script.js
+++ DOM and EVENT fundamentals/(Project01)GuessMyNumber/script.js
@@ -1,33 +1,42 @@
 'use strict';
-let RanNumber = Math.trunc(Math.random() * 18) + 2;
-let highScore = 0;
-let score = 20;
-let reset = document.querySelector('.reset')
-reset.addEventListener('click',function(){ 
-    location.reload();
+let RanNumber = Math.trunc(Math.random() * 18) + 2; // Random number generated on page refresh
+let highScore = 0; // The initial highscore
+let score = 20; // Total tries for the user before the game ends
+let bodyColor = document.querySelector('body');
+let result =document.querySelector('.result');
+// Event listner for reset button to refresh the page once the button clicked
+document.querySelector('.reset').addEventListener('click', function () {
+    location.reload(); //The location.reload() method in JavaScript is used to refresh or reload the current webpage.
 });
+//Event listner which checks the userInput is correct to the random number generated
 document.querySelector('.check').addEventListener('click', function () {
-    if ((document.querySelector('.userInput').value) !== '') {
-        score = score - 1;
-        if (score < 0) {
-            document.querySelector('.result').textContent = 'you lost the game try again';
-            return;
-        }
-        document.querySelector('body').style.backgroundColor = "#222";
-        const userInput = Number(document.querySelector('.userInput').value);
-        if (userInput === RanNumber) {
-            document.querySelector('.result').textContent = 'congrats you won';
-            highScore = highScore + userInput;
-            document.querySelector('.highScore').textContent = highScore;
-            document.querySelector('body').style.backgroundColor = "green";
-            RanNumber = Math.trunc(Math.random() * 18) + 2;
-        }
-        else if (userInput < RanNumber) {
-            document.querySelector('.result').textContent = 'too low!';
-        }
-        else {
-            document.querySelector('.result').textContent = 'too high!';
-        }
-        document.querySelector('.score').textContent = score;
+    if (score < 0) {
+        result.textContent = 'you lost the game try again';
+        return;
     }
+    bodyColor.style.backgroundColor = "#222";
+    const userInput = Number(document.querySelector('.userInput').value);
+    if (!userInput) {
+        result.textContent = ' Please enter the number';
+    }
+    // if the users guesses correct number 
+    else if (userInput === RanNumber) {
+        document.querySelector('#correctAnswer').textContent=RanNumber;
+        result.textContent = 'Congrats you won';
+        highScore = highScore + userInput;
+        document.querySelector('.highScore').textContent = highScore;
+        bodyColor.style.backgroundColor = "green";
+        RanNumber = Math.trunc(Math.random() * 18) + 2;
+    }
+    // if the users guesses less than the number 
+    else if (userInput < RanNumber) {
+        result.textContent = 'too low!';
+        score--;
+    }
+    // if the users guesses more than number 
+    else {
+        result.textContent = 'too high!';
+        score--;
+    }
+    document.querySelector('.score').textContent = score;
 });
