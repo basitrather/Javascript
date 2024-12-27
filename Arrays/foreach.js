84018c6c63e95b39611cb18daf4aaba7f9116fe9
basit showkat <118817679+basitrather@users.noreply.github.com>
2024-12-27 19:58:41 +0530

    Update foreach.js

--- Arrays/foreach.js
+++ Arrays/foreach.js
@@ -1,6 +1,3 @@
-forEach - Understanding how it works
-// ----------------------------------------------------------
-
 // The `forEach` method in JavaScript is used to execute a function for each element in an array.
 // It takes a callback function as an argument, and the callback function is executed for every element in the array.
 // The callback function can accept three parameters:
@@ -16,13 +13,4 @@ const numbers = [1, 2, 3, 4, 5];
 numbers.forEach(function (element, index) {
   console.log(`Element: ${element}, Index: ${index}`);
 });
-
-// OUTPUT:
-// Element: 1, Index: 0
-// Element: 2, Index: 1
-// Element: 3, Index: 2
-// Element: 4, Index: 3
-// Element: 5, Index: 4
-
-// In the example above, `forEach` executes the provided function for each item in the array. 
-// It takes the current element and its index as parameters and logs them to the console
+// To get better understanding how for each works first understand what is a callback function in function directory
