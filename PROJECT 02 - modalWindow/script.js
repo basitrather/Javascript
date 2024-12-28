4c6af69ae5b74c3da69f6bd53b9cdcd67a720928
thexzabuza <ratherbasit253@gmail.com>
2024-12-28 15:19:53 +0530

    File path changed and esc function added in project 02

--- PROJECT 02 - modalWindow/script.js
+++ PROJECT 02 - modalWindow/script.js
@@ -4,24 +4,31 @@ const modal = document.querySelectorAll('.modal');
 const hideModal = document.querySelectorAll('.hide-modal');
 const overlay = document.querySelector('.overlay');
 const btns = document.querySelector('.buttons');
-const removeHidden = function (i) {
+const hide = function (i) {
     modal[i].classList.add('hidden');
-    btns.classList.remove('hidden');
     overlay.classList.add('hidden');
+    btns.classList.remove('hidden');
 }
 const showHidden = function (i) {
     modal[i].classList.remove('hidden');
-    btns.classList.add('hidden');
     overlay.classList.remove('hidden');
+    btns.classList.add('hidden');
 }
 for (let i = 0; i < showBtn.length; i++) {
     showBtn[i].addEventListener('click', function () {
         showHidden(i);
     })
     hideModal[i].addEventListener('click', function () {
-        removeHidden(i);
+        hide(i);
     })
     overlay.addEventListener('click', function () {
-        removeHidden(i);
+        hide(i);
     })
+    document.addEventListener('keydown',function(press){
+        console.log(press.key);
+        if(press.key==='Escape'){
+            hide(i);
+        }
+    });
 }
+
