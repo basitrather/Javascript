03e77ee8039664dda0a5b970a0bbeb8b5019cdb3
basit showkat <118817679+basitrather@users.noreply.github.com>
2025-01-06 11:41:18 +0530

    Q2

--- Problem Solving Quests/06-01-2025/guessTheOutput.js
+++ Problem Solving Quests/06-01-2025/guessTheOutput.js
@@ -18,12 +18,6 @@ counter.increment();
 counter.decrement();
 counter.increment();
 
-// **Output**:
-// 1
-// 2
-// 1
-// 2
-
 // Output:
 // 1
 // 2
@@ -34,3 +28,20 @@ counter.increment();
 // - `increment()` increases `count` and remembers the updated value (closure retains state).
 // - `decrement()` decreases `count` and works similarly.
 // - Each call adjusts `count` based on the previous value, showcasing the behavior of closures.
+
+const outer = () => {
+  let outerVar = 5;
+
+  const inner = () => {
+    let innerVar = 10;
+    console.log(outerVar + innerVar);
+  };
+
+  return inner;
+};
+
+const myFunction = outer();
+myFunction();
+
+//output: 15
+// The inner can access the outer because its closure
