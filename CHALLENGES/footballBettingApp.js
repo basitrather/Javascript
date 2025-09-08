d42e24d3dad18aa39a987a8466c06683364b9e63
BasitRather <ratherbasit253@gmail.com>
2025-09-09 00:18:23 +0530

    Completed Challenge 1 succesfully

--- CHALLENGES/footballBettingApp.js
+++ CHALLENGES/footballBettingApp.js
@@ -11,3 +11,82 @@
 // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
 // TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const {
+  players: [player1, player2],
+} = game;
+
+const [gk, ...fieldPlayers] = player1;
+
+let allPlayers = [...player1, ...player2];
+
+const substitute = ["Thiago", "Coutinho", "Perisic"];
+const players1Final = [...player1, ...substitute];
+
+const {
+  odds: { team1, draw, team2 },
+} = game;
+
+const printGoals = function (scored, ...players) {
+  players.forEach((player) => {
+    let totalsGoals = 0;
+    let goalScorer = "";
+
+    scored.forEach((goal) => {
+      if (player === goal) {
+        totalsGoals++;
+        goalScorer = player;
+      }
+    });
+    if (totalsGoals > 0) {
+      console.log(`${player} scored ${totalsGoals} goals`);
+    }
+  });
+  console.log(`${scored.length} goals were scored in total`);
+};
+const { scored } = game;
+allPlayers = [...allPlayers, ...substitute];
+printGoals(scored, ...allPlayers);
+
+team1 < team2 && console.log(`${game.team1} is the winner of the season`);
+team2 < team1 && console.log(`${game.team2} is the winner of the season`);
