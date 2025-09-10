73acf254e23dceda45473acb105cf56927c6d5b8
BasitRather <ratherbasit253@gmail.com>
2025-09-10 18:11:23 +0530

    More set methods introduced in ES2025

--- Sets/setsIntro.js
+++ Sets/setsIntro.js
@@ -32,3 +32,39 @@ if we want to retrive data then array is the best datatype to go for*/
 const members = ["basit", "taureed", "tehsheem", "basit", "haseeb", "mohsin"]; // Duplicates
 const memebersUpdated = [...new Set(members)]; // created a Set with an array (removed duplicates), then spread it back into a new array.Then stored in the decleared varibale.
 console.log(memebersUpdated); //Updated array.
+
+//Data
+const italianFoods = new Set([
+  "pasta",
+  "gnocchi",
+  "tomatoes",
+  "olive oil",
+  "garlic",
+  "basil",
+]);
+
+const mexicanFoods = new Set([
+  "tortillas",
+  "beans",
+  "rice",
+  "tomatoes",
+  "avocado",
+  "garlic",
+]);
+// New set methods introduced in ES2025
+//Method 01: Intersection Method: This method finds the common elements between two sets.
+
+const commonFoods = mexicanFoods.intersection(italianFoods);
+console.log(commonFoods); //{'tomatoes', 'garlic'}
+
+//Method 01: Union Method: Joins two sets into one, excluding duplicates.
+
+const foodFusion = italianFoods.union(mexicanFoods);
+console.log(foodFusion); //{'pasta', 'gnocchi', 'tomatoes', 'olive oil', 'garlic','basil','tortillas','beans','rice','avocado'}
+
+//Method 03: Difference Method: method compares two sets and returns a new set containing elements that are present in the first set but not in the second set.
+
+const foodDifference = italianFoods.difference(mexicanFoods);
+console.log(foodDifference); //{'pasta', 'gnocchi', 'olive oil', 'basil'}
+
+//These are enough; the rest can be checked only if needed.
