2e024f72501bc52c458732a050020194878dc705
BasitRather <ratherbasit253@gmail.com>
2025-09-15 15:30:07 +0530

    Project almost Completed few things left to do

--- PROJECT 08 - EXPENSE TRACKER/script.js
+++ PROJECT 08 - EXPENSE TRACKER/script.js
@@ -1,4 +1,5 @@
 "use strict";
+//declearing variable
 const changeModeBtn = document.querySelector(".mode");
 const title = document.querySelector("h1");
 const label = document.querySelectorAll("label");
@@ -8,6 +9,7 @@ const expenseType = document.querySelector("#expenses");
 const submitExpenseBtn = document.querySelector(".addExpenseBtn");
 const tableBody = document.querySelector("#expense-table-body");
 const totalAmount = document.querySelector("#total-amount");
+const tableHeading = document.querySelectorAll("th");
 const userInputs = {
   description: "",
   amount: "",
@@ -16,24 +18,20 @@ const userInputs = {
 let totalSum = 0;
 
 // FUNCTIONS
-const toggleMode = function () {
-  document.body.classList.toggle("changeBackGroundImage");
-  title.classList.toggle("heading");
-  label.forEach((element) => {
-    element.classList.toggle("label");
-  });
-};
-const createElements = function () {
+//deleteItem function
+const deleteItem = function () {};
+// Create expense table on the DOM
+const createElements = function (object) {
   const tableRow = document.createElement("tr");
   tableBody.appendChild(tableRow);
   const td1 = document.createElement("td");
-  td1.textContent = userInputs.description;
+  td1.textContent = object.description;
   tableRow.appendChild(td1);
   const td2 = document.createElement("td");
-  td2.textContent = userInputs.amount;
+  td2.textContent = object.amount;
   tableRow.appendChild(td2);
   const td3 = document.createElement("td");
-  td3.textContent = userInputs.expenseType;
+  td3.textContent = object.expenseType;
   tableRow.appendChild(td3);
   const td4 = document.createElement("td");
   tableRow.appendChild(td4);
@@ -41,14 +39,38 @@ const createElements = function () {
   delBth.textContent = "Delete";
   delBth.classList.add("delete-btn");
   td4.appendChild(delBth);
-  totalSum += userInputs.amount;
+  totalSum += object.amount;
   totalAmount.textContent = totalSum;
   delBth.addEventListener("click", () => {
     tableRow.remove();
     totalSum -= td2.textContent;
     totalAmount.textContent = totalSum;
+    let objectArray = JSON.parse(localStorage.getItem("userData"));
+    objectArray = objectArray.filter(
+      (item) =>
+        !(
+          item.description === object.description &&
+          item.amount === object.amount &&
+          item.expenseType === object.expenseType
+        )
+    );
+    localStorage.setItem("userData", JSON.stringify(objectArray));
   });
 };
+//display the saved data
+const displayData = function () {
+  let objectArray = JSON.parse(localStorage.getItem("userData")) || [];
+  objectArray.forEach((element) => {
+    createElements(element);
+  });
+};
+//saving data in local storage
+const saveData = function () {
+  let userDataArray = JSON.parse(localStorage.getItem("userData")) || [];
+  userDataArray.push(userInputs);
+  localStorage.setItem("userData", JSON.stringify(userDataArray));
+};
+
 const getData = function () {
   userInputs.description = description.value;
   userInputs.amount = Number(amount.value);
@@ -56,16 +78,31 @@ const getData = function () {
   if (userInputs.description === "" || userInputs.amount == "") {
     alert("Enter the description and amount fields");
   } else {
-    createElements();
+    createElements(userInputs);
   }
+  saveData();
 };
 
-//EventListeners
-
-//ToggleModes
 submitExpenseBtn.addEventListener("click", () => {
   getData();
 });
-changeModeBtn.addEventListener("click", () => {
-  toggleMode();
-});
+
+displayData();
+
+const toggleThemeAutomatically = function () {
+  const tableData = document.querySelectorAll("td");
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.toggle("changeBackGroundImage");
+    title.classList.toggle("heading");
+    label.forEach((element) => {
+      element.classList.toggle("label");
+    });
+    tableHeading.forEach((element) => {
+      element.classList.toggle("borderStyling");
+    });
+    tableData.forEach((element) => {
+      element.classList.toggle("borderStyling");
+    });
+  }
+};
+toggleThemeAutomatically();
