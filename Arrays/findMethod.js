57e926d91a669289627accd1f19e095a558f7b0c
BasitRather <ratherbasit253@gmail.com>
2025-10-02 00:05:13 +0530

    introduction to findMethod

---
+++ Arrays/findMethod.js
@@ -0,0 +1,7 @@
+// Find method → Returns the first matching element (or undefined) and stops searching once found. while the filter method returns all the true vales
+const numbers = [3, 7, 10, 15, 20];
+
+const firstAbove10 = numbers.find((num) => num > 10);
+console.log(firstAbove10);
+
+//it is used to find a unique value in an array. sometimes it used for login purposes
