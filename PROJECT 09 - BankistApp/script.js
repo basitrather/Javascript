a080052cc4ba5adb03887d98f2c0a8e5a5526464
BasitRather <ratherbasit253@gmail.com>
2025-10-02 17:44:48 +0530

    Project 09- ownerName Updated

--- PROJECT 09 - BankistApp/script.js
+++ PROJECT 09 - BankistApp/script.js
@@ -1,28 +1,28 @@
 "use strict";
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "Basit Showkat Rather",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: "Arkam Ajaz",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5, //%
   pin: 2222,
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: "haseeb Dar",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7, //%
   pin: 3333,
 };
 
 const account4 = {
-  owner: "Sarah Smith",
+  owner: "Taureed Ayub",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1, //%
   pin: 4444,
