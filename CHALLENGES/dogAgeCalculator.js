a06a2023284d2ba0a14f10acb7e251e08747111d
BasitRather <ratherbasit253@gmail.com>
2025-10-03 16:25:19 +0530

    challenge completed DogAgeCalulator part03

--- CHALLENGES/dogAgeCalculator.js
+++ CHALLENGES/dogAgeCalculator.js
@@ -59,6 +59,7 @@ const calcAverageHumanAge = function (dogAges) {
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
+// Challenge 03
 // Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
 
 const calcAverageHumanAgeChain = function (dogAges) {
@@ -70,3 +71,97 @@ const calcAverageHumanAgeChain = function (dogAges) {
 };
 calcAverageHumanAgeChain([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAgeChain([16, 6, 10, 5, 6, 1, 4]);
+
+// Challenge 04
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+*/
+const breeds = [
+  {
+    breed: "German Shepherd",
+    averageWeight: 32,
+    activities: ["fetch", "swimming"],
+  },
+  {
+    breed: "Dalmatian",
+    averageWeight: 24,
+    activities: ["running", "fetch", "agility"],
+  },
+  {
+    breed: "Labrador",
+    averageWeight: 28,
+    activities: ["swimming", "fetch"],
+  },
+  {
+    breed: "Beagle",
+    averageWeight: 12,
+    activities: ["digging", "fetch"],
+  },
+  {
+    breed: "Husky",
+    averageWeight: 26,
+    activities: ["running", "agility", "swimming"],
+  },
+  {
+    breed: "Bulldog",
+    averageWeight: 36,
+    activities: ["sleeping"],
+  },
+  {
+    breed: "Poodle",
+    averageWeight: 18,
+    activities: ["agility", "fetch"],
+  },
+];
+// 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+const huskyWeight = breeds.find(
+  (object) => object.breed === "Husky"
+).averageWeight;
+
+//2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+const dogBothActivities = breeds.find(
+  (ele) =>
+    ele.activities.includes("running") && ele.activities.includes("fetch")
+).breed;
+
+// 3. Create an array "allActivities" of all the activities of all the dog breeds
+const allActivities = breeds.flatMap((ele) => ele.activities);
+
+//4.  Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+const uniqueActivities = [...new Set(allActivities)];
+
+// 5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+
+const swimmingAdjacent = [
+  ...new Set(
+    breeds
+      .filter((ele) => ele.activities.includes("swimming"))
+      .flatMap((ele) => ele.activities)
+      .filter((ele) => ele !== "swimming")
+  ),
+];
+
+// 6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+
+const age = breeds
+  .flatMap((ele) => ele.averageWeight)
+  .reduce((total, curren, _, arr) => total + curren / arr.length, 0);
+console.log(age >= 10);
+
+// 7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+const active = breeds
+  .map((ele) => ele.activities)
+  .some((activities) => activities.length == 3);
+console.log(active);
