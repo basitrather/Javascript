df800d9d8e14953f2d3d7d16cca3487ec531fd76
BasitRather <ratherbasit253@gmail.com>
2026-01-18 04:53:47 +0530

    Project 09 - Added major functionality and features

--- PROJECT 09 - BankistApp/Landing Page/lpsrc.js
+++ PROJECT 09 - BankistApp/Landing Page/lpsrc.js
@@ -1,9 +1,14 @@
 'use strict';
+
 const textBtn = document.querySelector('.btn--scroll-to');
 const navLinksList = document.querySelector('.nav__links');
 const tabBtns = document.querySelectorAll('.operations__tab');
 const tabBtnsContainer = document.querySelector('.operations__tab-container');
 const contentContainer = document.querySelectorAll('.operations__content');
+const navBar = document.querySelector('.nav');
+const section1 = document.querySelector('#section--1');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
 // Learn More button scroll effect
 textBtn.addEventListener('click', () => {
   document.querySelector('#section--1').scrollIntoView({ behavior: 'smooth' });
@@ -21,11 +26,9 @@ navLinksList.addEventListener('click', e => {
 });
 
 // Tabbed Component on operation sections
-
 tabBtnsContainer.addEventListener('click', e => {
   //Grabbing the event
   let tabClicked = e.target.closest('.operations__tab');
-  console.log(tabClicked);
   if (!tabClicked) return;
 
   //Remove classes
@@ -39,8 +42,102 @@ tabBtnsContainer.addEventListener('click', e => {
   //Adding classes
   document
     .querySelector(
-      `.operations__content--${tabClicked.getAttribute('data-tab')}`
+      `.operations__content--${tabClicked.getAttribute('data-tab')}`,
     )
     .classList.add('operations__content--active');
   tabClicked.classList.add('operations__tab--active');
+});
+
+//Animating the link fade out
+const fadeOut = function (ev) {
+  if (ev.target.classList.contains('nav__link')) {
+    const hoverOver = ev.target;
+    const linkSiblings = ev.target
+      .closest('.nav')
+      .querySelectorAll('.nav__link');
+    const logo = ev.target.closest('.nav').querySelector('img');
+    linkSiblings.forEach(element => {
+      if (element !== hoverOver) {
+        element.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+navBar.addEventListener('mouseover', fadeOut.bind(0.5));
+navBar.addEventListener('mouseout', fadeOut.bind(1));
+
+//Sticky scroll effect via window scroll (Old school way)
+// const initiateSecOne = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > initiateSecOne.top) {
+//     navBar.classList.add('sticky');
+//   } else {
+//     navBar.classList.remove('sticky');
+//   }
+// });
+
+//Sticky scroll effect via IntersectionObserver API
+
+const navHeight = navBar.getBoundingClientRect().height;
+const observerFunction = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    navBar.classList.add('sticky');
+  } else {
+    navBar.classList.remove('sticky');
+  }
+};
+const observerOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+const observer = new IntersectionObserver(observerFunction, observerOptions);
+observer.observe(header);
+
+//Revealing elements on scroll
+
+const revealSectionObsFn = function (entries, observer) {
+  entries.forEach(viewportEntry => {
+    if (viewportEntry.isIntersecting)
+      viewportEntry.target.classList.remove('section--hidden');
+    if (!viewportEntry.isIntersecting)
+      viewportEntry.target.classList.add('section--hidden');
+    // observer.unobserve(viewportEntry.target);
+  });
+};
+const revealSectionOpt = {
+  root: null,
+  threshold: 0.15,
+};
+const revealSectionObserver = new IntersectionObserver(
+  revealSectionObsFn,
+  revealSectionOpt,
+);
+allSections.forEach(element => {
+  revealSectionObserver.observe(element);
+  element.classList.add('section--hidden');
+});
+
+//Lazy loading the Images
+const lazyImages = document.querySelectorAll('img[data-src]');
+const loadImageFn = function (entries, observer) {
+  const [imageEntry] = entries;
+  if (!imageEntry.isIntersecting) return;
+
+  //Lazy loading images
+  imageEntry.target.src = imageEntry.target.dataset.src;
+  imageEntry.target.addEventListener('load', function () {
+    imageEntry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(imageEntry.target);
+};
+const imageObserver = new IntersectionObserver(loadImageFn, {
+  root: null,
+  threshold: 0,
+  rootMargin: `150px`,
+});
+lazyImages.forEach(ele => {
+  imageObserver.observe(ele);
 });
